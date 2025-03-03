e584a53584de716e550fad0127ed258074029c32
ashodkh <ask126@pitt.edu>
2025-03-03 17:14:17 -0500

    added dropdown menu for color coding

--- dash_script_images.py
+++ dash_script_images.py
@@ -10,7 +10,8 @@ load_figure_template(["darkly"])
 
 
 def dash_plot_images(x=None, y=None, xlim=None, ylim=None, color_code=None,
-                     cmap_plot='Viridis', images=None, cmap_images='inferno',
+                     cmap_plot='Viridis', marker_size=10,
+                     images=None, cmap_images='inferno',
                      image_labels=None):
     '''
     Plotting function that uses Dash to plot galaxies in a 2d plane of properties and shows their spectra by hovering over the points.
@@ -30,16 +31,19 @@ def dash_plot_images(x=None, y=None, xlim=None, ylim=None, color_code=None,
 
     color_code: Python dictionary
                 Array values to color code the points in the 2D plane.
-                The keys are the labels for the color-coding.
+                The keys are the labels for the color-coding. Shows up in a dropdown menu.
           
     cmap_plot: String. Default is 'Viridis'
                The color map used to color-code the x,y points with color_code values.
 
-    images: List of 3D arrays (N_pixe, N_pixel, N_points)
-            A list of Images to be plotted.
-            Can input several images for each object: images[0] should be a 3D array (N_pixe, N_pixel, N_points).
+    marker_size: Integer. Default is 10
+                 The size of the markers in the 2D plane.
 
-    cmap_plot: String. Default is 'Viridis'
+    images: List of 3D arrays (N_points, N_pixe, N_pixel)
+            A list of Images to be plotted.
+            Can input several images for each object: images[0] should be a 3D array (N_points, N_pixe, N_pixel).
+
+    cmap_images: String. Default is 'Viridis'
                The color map used for the images.
 
     image_labels: List of strings
@@ -62,6 +66,7 @@ def dash_plot_images(x=None, y=None, xlim=None, ylim=None, color_code=None,
         mode='markers',
         name='galaxies',
         marker=dict(
+            size=marker_size,  # Change this value to adjust marker size
             color=list(color_code.values())[0],
             colorscale=cmap_plot,
             colorbar=dict(
@@ -75,7 +80,16 @@ def dash_plot_images(x=None, y=None, xlim=None, ylim=None, color_code=None,
     fig.update_yaxes(title=list(y.keys())[0], range=ylim)
     fig.update_layout(width=750, height=650, font=dict(size=30))
 
+
     app.layout = html.Div([
+        html.Div([
+            dcc.Dropdown(
+                options=[{'label': html.Span(k, style={'color': 'white'}), 'value': k} for k in color_code.keys()],
+                value=list(color_code.keys())[0],
+                id='color coding',
+                style={'width': '50%', 'backgroundColor': '#2a2a2a'}
+            )
+        ]),
         html.Div([
             dcc.Graph(id='2d-scatter', figure=fig,
                       style={'display': 'inline-block'}, mathjax=True)
@@ -91,7 +105,7 @@ def dash_plot_images(x=None, y=None, xlim=None, ylim=None, color_code=None,
         figs = []
         for i in range(len(images)):
             fig0 = px.imshow(images[i][ind,:,:], color_continuous_scale=cmap_images)
-            fig0.update_layout(title=str(ind), font=dict(size=30), title_x=0.5)
+            #fig0.update_layout(title=str(ind), font=dict(size=30), title_x=0.5)
             figs.append(fig0)
         return figs
 
@@ -99,7 +113,6 @@ def dash_plot_images(x=None, y=None, xlim=None, ylim=None, color_code=None,
         [Output(image_labels[l], 'figure') for l in range(len(images))],
         Input('2d-scatter', 'hoverData')
     )
-    
     def update_spectrum(hov_data):
         if hov_data is None:
             ind = 0
@@ -107,4 +120,29 @@ def dash_plot_images(x=None, y=None, xlim=None, ylim=None, color_code=None,
             ind = hov_data['points'][0]['pointIndex']
         return create_images(ind)
 
+    @app.callback(
+        Output('2d-scatter', 'figure'),
+        Input('color coding', 'value')
+    )
+    def update_color_coding(color):
+        fig = go.Figure()
+        trace0 = go.Scatter(
+            x=list(x.values())[0], y=list(y.values())[0],
+            mode='markers',
+            name='galaxies',
+            marker=dict(
+                size=marker_size,  # Change this value to adjust marker size
+                color=list(color_code[color]),
+                colorscale=cmap_plot,
+                colorbar=dict(
+                    title=color, orientation='h'
+                )
+            )
+        )
+        fig.add_trace(trace0)
+        
+        return fig
+
     return app
+
+    
